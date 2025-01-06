516eb0db4b1825ab6e489f58b7ecc655be1717da
Mayling Chen <Mayling00.C@gmail.com>
2025-01-06 14:18:20 +0800

    update

--- vignettes/Untitled.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-data(iris)
-head(iris)
-ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, col=Species)) +
-  coord_fixed() +
-  geom_point(size = 2) +
-  theme_bw() + scale_color_manual(values = rainbow(length(unique(iris$Species))))
